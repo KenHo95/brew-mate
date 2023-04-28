9bf9d1e5474b6725b4b08d4636bf1248fdf60c6a
KenHo95 <mingquan.ho.2017@accountancy.smu.edu.sg>
2023-04-28 22:10:54 +0800

    Move Recipe page 1 from app.js to CoffeeRecipePage.js, Changed code to pass states from app.js to CoffeeRecipePage.js child component as props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
-import logo from "./coffee-garden-ai.jpg";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Table from "react-bootstrap/Table";
+import CoffeeRecipePage from "./pages/CoffeeRecipePage";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      coffee: 0,
-      water: 0,
-      grind: 0,
-      time: 0,
+      recipeCoffeeGrams: 1,
+      recipeWaterML: 2,
+      recipeGrindType: "Fine-Coarse",
+      recipeTime: "3min",
     };
   }
 
@@ -19,33 +17,12 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Table bordered hover style={{ color: "white", width: "400px" }}>
-            <thead>
-              <tr>
-                <th>Coffee</th>
-                <th>Water</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  {this.state.coffee === 0 ? "-" : this.state.coffee + "g"}
-                </td>
-                <td>
-                  {this.state.water === 0 ? "-" : this.state.coffee + "ml"}
-                </td>
-              </tr>
-              <tr>
-                <th>Grind</th>
-                <th>Time</th>
-              </tr>
-              <tr>
-                <td>{this.state.grind === 0 ? "-" : this.state.coffee}</td>
-                <td>{this.state.time === 0 ? "-" : this.state.coffee}</td>
-              </tr>
-            </tbody>
-          </Table>
+          <CoffeeRecipePage
+            recipeCoffeeGrams={this.state.recipeCoffeeGrams}
+            recipeWaterML={this.state.recipeWaterML}
+            recipeGrindType={this.state.recipeGrindType}
+            recipeTime={this.state.recipeTime}
+          />
         </header>
       </div>
     );
---
+++ src/pages/CoffeeRecipePage.js
@@ -0,0 +1,52 @@
+import React from "react";
+import logo from "../coffee-garden-ai.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Table from "react-bootstrap/Table";
+
+class CoffeeRecipePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Table bordered hover style={{ color: "white", width: "400px" }}>
+          <thead>
+            <tr>
+              <th>Coffee</th>
+              <th>Water</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                {this.props.recipeCoffeeGrams === 0
+                  ? "-"
+                  : this.props.recipeCoffeeGrams + " g"}
+              </td>
+              <td>
+                {this.props.recipeWaterML === 0
+                  ? "-"
+                  : this.props.recipeWaterML + " ml"}
+              </td>
+            </tr>
+            <tr>
+              <th>Grind</th>
+              <th>Time</th>
+            </tr>
+            <tr>
+              <td>
+                {this.props.recipeGrindType === 0
+                  ? "-"
+                  : this.props.recipeGrindType}
+              </td>
+              <td>
+                {this.props.recipeTime === 0 ? "-" : this.props.recipeTime}
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+}
+
+export default CoffeeRecipePage;
