b42f4f26a494cc5c9402383c51bd19ec823455bb
KenHo95 <mingquan.ho.2017@accountancy.smu.edu.sg>
2023-05-01 08:58:31 +0800

    Added placeholder timer page to hold placeholder timer display component, Added StartStopbutton handle logic to switch between recipe page and timer page

---
+++ src/components/TimerDisplay.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class TimerDisplay extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default TimerDisplay;
