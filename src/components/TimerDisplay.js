b78ea23b585206b56e6b55cc826a91c446a1ac85
KenHo95 <mingquan.ho.2017@accountancy.smu.edu.sg>
2023-05-01 15:42:34 +0800

    Added basic timer display to display elapsed time in minutes and seconds after user starts timer

--- src/components/TimerDisplay.js
+++ src/components/TimerDisplay.js
@@ -2,8 +2,34 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class TimerDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { timeElapsed: 55 };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      timeElapsed: this.state.timeElapsed + 1,
+    });
+  }
   render() {
-    return <div></div>;
+    const minElapsed = Math.floor(this.state.timeElapsed / 60);
+    const secElapsed = this.state.timeElapsed % 60;
+
+    return (
+      <div>
+        <h2>Time Elapsed</h2>
+        {minElapsed >= 1 ? minElapsed + " min " : "0 min"} {secElapsed} sec
+      </div>
+    );
   }
 }
 
